9be8ecd3060dd8df250b8c27fb807cf076655117
Matheus <35335429+matheusvlribeiro@users.noreply.github.com>
2023-05-29 17:07:15 -0300

    Update 3_visao_restaurante_mod.py
    timestamps

--- pages/3_visao_restaurante_mod.py
+++ pages/3_visao_restaurante_mod.py
@@ -134,9 +134,9 @@ st.sidebar.markdown('''---''')
 st.sidebar.markdown('## Selecione uma data limite')
 date_slider = st.sidebar.slider(
     'Até qual valor?', 
-    value = pd.datetime(2022, 4, 13), 
-    min_value = pd.datetime(2022,2, 11), 
-    max_value = pd.datetime(2022, 4, 6), 
+    value = pd.Timestamp(2022, 4, 13).to_pydatetime(), 
+    min_value = pd.Timestamp(2022,2, 11).to_pydatetime(), 
+    max_value = pd.Timestamp(2022, 4, 6).to_pydatetime(), 
     format = 'DD-MM-YYYY')
 
 st.header(date_slider)
